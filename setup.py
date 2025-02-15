2e7ac0e0162ba189bf4e7dbba7393cb3a0f0a30b
hexgrad <166769057+hexgrad@users.noreply.github.com>
2025-02-14 22:49:19 -0800

    Bump to 0.7.16 (#94)

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='kokoro',
-    version='0.7.15',
+    version='0.7.16',
     packages=find_packages(),
     install_requires=[
         'huggingface_hub',
         'loguru',
-        'misaki[en]>=0.7.15',
+        'misaki[en]>=0.7.16',
         'numpy==1.26.4',
         'scipy',
         'torch',
