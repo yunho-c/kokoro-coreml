dd95acac904135f887648ff4a818d12af1d86272
szsteven008 <97944818+szsteven008@users.noreply.github.com>
2025-03-01 03:01:34 +0800

    add onnx export.py (#112)
    * Add files via upload
    
    onnx export
    
    * Add files via upload
    
    KModelForONNX
    
    * Add files via upload
    
    * Delete export.py
    
    * Add files via upload
    
    * Add files via upload
    
    修正中文的错误
    
    * Add files via upload
    
    增加duration的输出

--- kokoro/model.py
+++ kokoro/model.py
@@ -148,4 +148,4 @@ class KModelForONNX(torch.nn.Module):
         speed: float = 1
     ) -> tuple[torch.FloatTensor, torch.LongTensor]:
         waveform, duration = self.kmodel.forward_with_tokens(input_ids, ref_s, speed)
-        return waveform
+        return waveform, duration
